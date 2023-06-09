b403777ce8847ffb82c32bf1d2933517078c679c
peterjxl <peterjxl@qq.com>
2023-06-10 07:50:36 +0800

    演示Quartz的持久化

--- src/main/java/com/peterjxl/learnquartz/jobconfig/JobConfig.java
+++ src/main/java/com/peterjxl/learnquartz/jobconfig/JobConfig.java
@@ -2,15 +2,24 @@ package com.peterjxl.learnquartz.jobconfig;
 
 
 import com.peterjxl.learnquartz.demo8.SpringJob1;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
+import org.quartz.*;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JobConfig {
+
+    @Bean
+    @QuartzDataSource
+    public DataSource quartzDataSource(){
+        return new DriverManagerDataSource("jdbc:mysql://localhost:3306/learnQuartz?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC",
+                "learnQuartzUser",
+                "learnQuartzPassword");
+    }
 
     @Bean
     public JobDetail springJobDetail(){
@@ -25,6 +34,9 @@ public class JobConfig {
         return TriggerBuilder.newTrigger()
                 .forJob("springJob1Detail")
                 .startNow()
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(5)
+                        .repeatForever())
                 .build();
     }
 
--- src/main/java/com/peterjxl/learnquartz/service/HelloSpringService.java
+++ src/main/java/com/peterjxl/learnquartz/service/HelloSpringService.java
@@ -1,10 +1,22 @@
 package com.peterjxl.learnquartz.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 
 @Service
 public class HelloSpringService {
+
+    @Autowired
+    DataSource dataSource;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("dataSource: " + dataSource);
+    }
 
     public String  helloSpring() {
         return "Hello Spring";
